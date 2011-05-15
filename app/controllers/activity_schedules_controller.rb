599d9081de4dce5db34f55fd926198385f9d9e87
David Wilkie <dwilkie@gmail.com>
2011-05-15 09:06:38 +0700

    Add location to the activity description

--- app/controllers/activity_schedules_controller.rb
+++ app/controllers/activity_schedules_controller.rb
@@ -1,7 +1,7 @@
 class ActivitySchedulesController < ApplicationController
   def index
     day = Day.find(params[:day_id])
-    @activities = day.activities.includes(:images)
+    @activities = day.activities.includes(:images, :location)
   end
 end
 
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,9 @@
 class Location < ActiveRecord::Base
   validates :name, :uniqueness => true
   has_many :activities
+
+  def to_s
+    name
+  end
 end
 
