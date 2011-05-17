2820be65536f3d64f8df85af59cf4d3f5dae4daf
David Wilkie <dwilkie@gmail.com>
2011-05-18 00:25:35 +0800

    added relationship between location and days

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,8 @@
 class Location < ActiveRecord::Base
   validates :name, :uniqueness => true
   has_many :activities
+  has_many :activity_schedules, :through => :activities
+  has_many :days, :through => :activity_schedules
 
   def to_s
     name
