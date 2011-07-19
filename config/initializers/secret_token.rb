88022318d4e776a6db163ff7cc98f7bb6001ee0f
David Wilkie <dwilkie@gmail.com>
2011-07-19 13:21:07 +0700

    new refinery app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaraTravel::Application.config.secret_token = '3f540168d2808f746bd5d26a9c1b2aaa0ca229491e2cd038a5b245ab799b437ea6679028a368c7af7fd2922b9241321cf31985548c162768841cd62700ed87c5'
